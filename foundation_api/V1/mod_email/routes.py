b544be8722ec12299dc9cee555c3e12e896f9a7b
Nicolas Arnold <nicolas.arnold@protonmail.com>
2021-04-28 16:54:37 -0600

    add email receive action to db in parse_email

--- foundation_api/V1/mod_email/routes.py
+++ foundation_api/V1/mod_email/routes.py
@@ -1,15 +1,17 @@
 import email
+import json
 import os
 import random
 import string
 from datetime import datetime, timedelta, timezone
 from threading import Thread
 from uuid import uuid4
-import json
 
 from flask import Blueprint, jsonify, make_response, request
 from flask_jwt_extended import get_jwt, get_jwt_identity, jwt_required
 from foundation_api import app, bcrypt, db, mail
+from foundation_api.V1.mod_email.models import Action
+from sqlalchemy import and_, or_
 
 mod_email = Blueprint('email', __name__, url_prefix='/api/v1')
 
@@ -52,8 +54,23 @@ def parse_email():
     # print(to_addr)
 
     # Gmail get original email message id
-    print(email_message.get('In-Reply-To'))
-    print(email_message.get('References'))
+    # print(email_message.get('In-Reply-To'))
+    # print(email_message.get('References'))
+
+    # O365 get original email message id
+    references = str(email_message.get('References')).split(',')
+    for reference in references:
+        print(reference)
+        if original_send_action := db.session.query(Action).filter(and_(Action.email_message_id == reference, Action.action_type_id == 4)).first():
+            if original_receive_action := db.session.query(Action).filter(and_(Action.email_message_id == reference, Action.action_type_id == 6)).first():
+                pass
+            else:
+                new_action = Action(str(uuid4()), original_send_action.contact_id, 6, datetime.utcnow(), None, None, reference)
+                db.session.add(new_action)
+                db.session.commit()
+    # original_message_id = references.split(',')
+    # print(original_message_id)
+    # print(type(original_message_id))
 
     
 
