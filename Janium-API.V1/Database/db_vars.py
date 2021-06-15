cdcb2fb80940a3eeb80ed2426adc04146f1da1b4
Nicolas Arnold <nicolas.arnold@protonmail.com>
2021-06-15 09:38:10 -0600

    remove deprecated and old api

--- Janium-API.V1/Database/db_vars.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime,timezone,date
-class DatabaseVars(object):
-    api_user_id='37acdd30-8456-11eb-b788-8c85908d07de'
-    jason_user_id='e98f3c45-2f62-11eb-865a-42010a3d0005'
-    dt=datetime.utcnow()
-    dt_format=dt.strftime("%Y-%m-%d %H:%M:%S")
-    jason_account_id='e98f3c45-2f62-11eb-865a-42010a3d0004'
-    date=dt.strftime("%Y-%m-%d")
-    email_server_id='e98f3c45-2f62-11eb-865a-42010a3d0006'
