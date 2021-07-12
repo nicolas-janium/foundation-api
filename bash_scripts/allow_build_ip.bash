276cd35ce37a278d6df2f954fca1a41ed40e3ebe
Nicolas Arnold <nicolas.arnold@protonmail.com>
2021-07-12 09:13:43 -0600

    update home ip address

--- bash_scripts/allow_build_ip.bash
+++ bash_scripts/allow_build_ip.bash
@@ -1,8 +1,8 @@
 if [ "$2" == "set" ]; then
     echo "Setting db server's authorized networks"
     ip=$(curl ifconfig.me)
-    gcloud sql instances patch $1 --authorized-networks="67.176.83.65","${ip}"
+    gcloud sql instances patch $1 --authorized-networks="67.161.195.143","${ip}"
 else
     echo "Resetting db server's authorized networks"
-    gcloud sql instances patch $1 --authorized-networks="67.176.83.65"
+    gcloud sql instances patch $1 --authorized-networks="67.161.195.143"
 fi 
