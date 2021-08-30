44ff93520badca86b17b775f935244fee5678978
Nicolas Arnold <nicolas.arnold@protonmail.com>
2021-08-30 15:29:40 -0600

    comment parse_email route

--- foundation_api/V1/mod_email/routes.py
+++ foundation_api/V1/mod_email/routes.py
@@ -202,63 +202,63 @@ def is_ses_identity_dkim_verified_route():
         return jsonify({"message": False})
     return jsonify({"message": "Unknown email_config_id"})
 
-@mod_email.route('/parse_email', methods=['POST'])
-def parse_email():
-    """
-    Required JSON keys: None
-    """
-    req_dict = request.form.to_dict()
-    # pprint(req_dict)
-    email_message = email.message_from_string(req_dict['email'])
+# @mod_email.route('/parse_email', methods=['POST'])
+# def parse_email():
+#     """
+#     Required JSON keys: None
+#     """
+#     req_dict = request.form.to_dict()
+#     # pprint(req_dict)
+#     email_message = email.message_from_string(req_dict['email'])
 
-    body = ''
-    if email_message.is_multipart():
-        for part in email_message.walk():
-            ctype = part.get_content_type()
-            cdispo = str(part.get('Content-Disposition'))
+#     body = ''
+#     if email_message.is_multipart():
+#         for part in email_message.walk():
+#             ctype = part.get_content_type()
+#             cdispo = str(part.get('Content-Disposition'))
 
-            if ctype == 'text/plain' and 'attachment' not in cdispo:
-                body = part.get_payload(decode=True)  # decode
-            elif ctype == 'text/html' and 'attachment' not in cdispo:
-                body = part.get_payload(decode=True)  # decode
-                break # Default to html body
-    else:
-        body = email_message.get_payload(decode=True)
+#             if ctype == 'text/plain' and 'attachment' not in cdispo:
+#                 body = part.get_payload(decode=True)  # decode
+#             elif ctype == 'text/html' and 'attachment' not in cdispo:
+#                 body = part.get_payload(decode=True)  # decode
+#                 break # Default to html body
+#     else:
+#         body = email_message.get_payload(decode=True)
 
-    is_from_outlook = True if 'outlook' in email_message.get('Message-ID') else False
-    to_address = str(email_message.get('To'))
-    index = to_address.index('<')
-    to_address = to_address[index + 1: len(to_address) - 1]
+#     is_from_outlook = True if 'outlook' in email_message.get('Message-ID') else False
+#     to_address = str(email_message.get('To'))
+#     index = to_address.index('<')
+#     to_address = to_address[index + 1: len(to_address) - 1]
 
-    forwarded_to_address = str(email_message.get('X-Forwarded-To'))
+#     forwarded_to_address = str(email_message.get('X-Forwarded-To'))
 
-    ### For gmail, inbound_parse_email is in X-Forwarded-To field. For O365, it's in the To field ###
-    if 'Janium Forwarding Rule Test Email' in json.dumps(req_dict):
-        if email_config := db.session.query(Email_config).filter(Email_config.inbound_parse_email.in_([to_address, forwarded_to_address])).first():
-            email_config.is_email_forwarding_rule_verified = True
-            flag_modified(email_config, 'is_email_forwarding_rule_verified')
-            db.session.commit()
-    elif 'forwarding-noreply@google.com' in json.dumps(req_dict) or 'Gmail Forwarding Confirmation' in json.dumps(req_dict):
-        if email_config := db.session.query(Email_config).filter(Email_config.inbound_parse_email.in_([to_address, forwarded_to_address])).first():
-            body = str(body)
-            confirmation_code_index = body.index('Confirmation code')
-            confirmation_code = body[confirmation_code_index + 19: confirmation_code_index + 28]
-            email_config.gmail_forwarding_confirmation_code = confirmation_code
-            flag_modified(email_config, 'gmail_forwarding_confirmation_code')
-            db.session.commit()
-    else:
-        references = str(email_message.get('References')).split(',')
-        for reference in references:
-            reference = str(reference).replace('<', '').replace('>', '').split('@')[0]
-            if original_send_action := db.session.query(Action).filter(and_(Action.email_message_id == reference, Action.action_type_id == 4)).first():
-                if original_receive_action := db.session.query(Action).filter(and_(Action.email_message_id == reference, Action.action_type_id == 6)).first():
-                    pass
-                else:
-                    new_action = Action(str(uuid4()), original_send_action.contact_id, 6, datetime.utcnow(), None, None, reference)
-                    db.session.add(new_action)
-                    db.session.commit()
+#     ### For gmail, inbound_parse_email is in X-Forwarded-To field. For O365, it's in the To field ###
+#     if 'Janium Forwarding Rule Test Email' in json.dumps(req_dict):
+#         if email_config := db.session.query(Email_config).filter(Email_config.inbound_parse_email.in_([to_address, forwarded_to_address])).first():
+#             email_config.is_email_forwarding_rule_verified = True
+#             flag_modified(email_config, 'is_email_forwarding_rule_verified')
+#             db.session.commit()
+#     elif 'forwarding-noreply@google.com' in json.dumps(req_dict) or 'Gmail Forwarding Confirmation' in json.dumps(req_dict):
+#         if email_config := db.session.query(Email_config).filter(Email_config.inbound_parse_email.in_([to_address, forwarded_to_address])).first():
+#             body = str(body)
+#             confirmation_code_index = body.index('Confirmation code')
+#             confirmation_code = body[confirmation_code_index + 19: confirmation_code_index + 28]
+#             email_config.gmail_forwarding_confirmation_code = confirmation_code
+#             flag_modified(email_config, 'gmail_forwarding_confirmation_code')
+#             db.session.commit()
+#     else:
+#         references = str(email_message.get('References')).split(',')
+#         for reference in references:
+#             reference = str(reference).replace('<', '').replace('>', '').split('@')[0]
+#             if original_send_action := db.session.query(Action).filter(and_(Action.email_message_id == reference, Action.action_type_id == 4)).first():
+#                 if original_receive_action := db.session.query(Action).filter(and_(Action.email_message_id == reference, Action.action_type_id == 6)).first():
+#                     pass
+#                 else:
+#                     new_action = Action(str(uuid4()), original_send_action.contact_id, 6, datetime.utcnow(), None, None, reference)
+#                     db.session.add(new_action)
+#                     db.session.commit()
 
-    return jsonify({"message": "text"})
+#     return jsonify({"message": "text"})
 
 @mod_email.route('/sns', methods=['POST'])
 def catch_sns():
