14a5dbc6ad077ce5387ec3fec32d01f2b63f97ef
Nicolas Arnold <nicolas.arnold@protonmail.com>
2021-08-30 14:40:44 -0600

    increase memory size of process-cs function

--- bash_scripts/deploy_functions.bash
+++ bash_scripts/deploy_functions.bash
@@ -15,7 +15,7 @@ do
 
     if [ ${func_name} == 'process-contact-source-task-handler' ]
     then
-        gcloud functions deploy ${func_name} --region=${1} --entry-point=main --runtime=python38 --trigger-http --allow-unauthenticated --timeout=539 --env-vars-file=./functions/envs.yaml --source=${func_folder} --egress-settings=private-ranges-only --vpc-connector=${2} --memory=1024MB
+        gcloud functions deploy ${func_name} --region=${1} --entry-point=main --runtime=python38 --trigger-http --allow-unauthenticated --timeout=539 --env-vars-file=./functions/envs.yaml --source=${func_folder} --egress-settings=private-ranges-only --vpc-connector=${2} --memory=2048MB
     elif [ ${func_name} == 'parse-email-task-handler' ]
     then
         gcloud functions deploy ${func_name} --region=${1} --entry-point=main --runtime=python38 --trigger-http --allow-unauthenticated --timeout=539 --env-vars-file=./functions/envs.yaml --source=${func_folder} --egress-settings=private-ranges-only --vpc-connector=${2} --memory=512MB
