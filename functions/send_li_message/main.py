eabbb85f12c9e553c775de73c90101a50f3aab03
Nicolas Arnold <nicolas.arnold@protonmail.com>
2021-09-11 11:33:32 -0600

    add poll webhook response to send_li_message

--- functions/send_li_message/main.py
+++ functions/send_li_message/main.py
@@ -1,17 +1,36 @@
-from unittest.mock import Mock
-import os
 import json
+import os
+from unittest.mock import Mock
+from uuid import uuid4
+from datetime import datetime, timedelta
 
 import requests
 import urllib3
 from bs4 import BeautifulSoup as Soup
 from flask import Response
 from google.cloud import tasks_v2
+from google.protobuf import timestamp_pb2
+from urllib3.exceptions import InsecureRequestWarning
 
-from model import (Contact, Ulinc_config, Ulinc_campaign, Janium_campaign_step, create_gcf_db_engine,
+from model import (Contact, Contact_source, Janium_campaign_step,
+                   Ulinc_campaign, Ulinc_config, create_gcf_db_engine,
                    create_gcf_db_session)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning) # pylint: disable=no-member
+
+
+def poll_and_save_webhook(ulinc_config, wh_url, wh_type, session):
+    res = requests.get(wh_url, verify=False)
+    if res.ok:
+        if res_json := res.json():
+            contact_source = Contact_source(str(uuid4()), ulinc_config.ulinc_config_id, wh_type, res_json)
+            session.add(contact_source)
+            session.commit()
+            return "Webhook response saved"
+        return "Webhook response empty"
+    return "Bad request"
 
  
 def insert_key_words(email_html, key_words_dict):
@@ -70,27 +89,53 @@ def main(request):
                 if ulinc_campaign := session.query(Ulinc_campaign).filter(Ulinc_campaign.ulinc_campaign_id == json_body['ulinc_campaign_id']).first():
                     if contact := session.query(Contact).filter(Contact.contact_id == json_body['contact_id']).first():
                         if contact.is_messaging_task_valid():
-                            send_li_message_res = send_li_message(ulinc_config, janium_campaign_step, ulinc_campaign, contact)
-                            if send_li_message_res == 'Success':
-                                payload = {
-                                    "ulinc_config_id": ulinc_config.ulinc_config_id,
-                                    "ulinc_campaign_id": ulinc_campaign.ulinc_campaign_id,
-                                    "contact_id": contact.contact_id
-                                }
+                            webhook_response = poll_and_save_webhook(ulinc_config, ulinc_config.new_message_webhook, 2, session)
+                            if webhook_response == 'Webhook response saved':
+                                payload = json_body
                                 task = {
                                     "http_request": {  # Specify the type of request.
                                         "http_method": tasks_v2.HttpMethod.POST,
-                                        "url": os.getenv('UPDATE_ULINC_CONTACT_STATUS_TRIGGER_URL'),
+                                        "url": os.getenv('SEND_LI_MESSAGE_TRIGGER_URL'),
                                         'body': json.dumps(payload).encode(),
                                         'headers': {
                                             'Content-type': 'application/json'
                                         }
                                     }
                                 }
-                                task_response = gct_client.create_task(parent=gct_parent, task=task)
-                                return Response('Success', 200)
+
+                                # Add the timestamp to the tasks.
+                                timestamp = timestamp_pb2.Timestamp()
+                                timestamp.FromDatetime(datetime.utcnow() + timedelta(minutes=15))
+                                task['schedule_time'] = timestamp
+
+                                gct_send_parent = gct_client.queue_path(os.getenv('PROJECT_ID'), os.getenv('TASK_QUEUE_LOCATION'), queue='send-li-message')
+                                task_response = gct_client.create_task(parent=gct_send_parent, task=task)
+                                return Response('New message webhook response was not empty. Created new task', 200)
+
+                            elif webhook_response == 'Webhook response empty':
+                                send_li_message_res = send_li_message(ulinc_config, janium_campaign_step, ulinc_campaign, contact)
+                                if send_li_message_res == 'Success':
+                                    payload = {
+                                        "ulinc_config_id": ulinc_config.ulinc_config_id,
+                                        "ulinc_campaign_id": ulinc_campaign.ulinc_campaign_id,
+                                        "contact_id": contact.contact_id
+                                    }
+                                    task = {
+                                        "http_request": {  # Specify the type of request.
+                                            "http_method": tasks_v2.HttpMethod.POST,
+                                            "url": os.getenv('UPDATE_ULINC_CONTACT_STATUS_TRIGGER_URL'),
+                                            'body': json.dumps(payload).encode(),
+                                            'headers': {
+                                                'Content-type': 'application/json'
+                                            }
+                                        }
+                                    }
+                                    task_response = gct_client.create_task(parent=gct_parent, task=task)
+                                    return Response('Success', 200)
+                                else:
+                                    return Response(send_li_message_res['error_message'], 200) # Should not retry
                             else:
-                                return Response(send_li_message_res['error_message'], 200) # Should not retry
+                                pass
                         return Response("Messaging task no longer valid", 200)
                     return Response("Unknown contact_id", 200)
                 return Response("Unknown ulinc_campaign_id", 200)
